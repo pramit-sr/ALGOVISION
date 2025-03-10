06f64384589111e48506a10c858d66062a804ff5
pramit_sr <mepramitsarkarr@gmail.com>
2025-03-10 23:01:15 +0530

    changes

--- AlgoVision/src/constants/index.js
+++ AlgoVision/src/constants/index.js
@@ -27,6 +27,48 @@ export const navLinks = [
   },
 ];
 
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
 
 export const footerLinks = [
   {
@@ -34,7 +76,7 @@ export const footerLinks = [
     links: [
       {
         name: "Jaypee University of Engineering & Technology",
-        link: "https://www.juet.ac.in/",     
+       /* link: "https://www.hoobank.com/content/",     */
       },
     ],
   },

3c4944c9c12e3a37c9dbdf11104e20defe251835
pramit_sr <mepramitsarkarr@gmail.com>
2025-04-18 21:45:00 +0530

    update in Page.jsx

--- AlgoVision/src/components/Page.jsx
+++ AlgoVision/src/components/Page.jsx
@@ -25,23 +25,6 @@ const FeatureCard = ({ icon, title, content, index }) => (
 
 const Page = () => {
   return (
-    /*<section id='features' className={layout.section}>
-      <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden'/>we'll handle the money.</h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          With the right credit card, 
-          you can improve your financial life by building credit,
-          earning rewards and saving money.
-          But with hundreds of credit cards on the market.
-        </p>
-        <Button styles='mt-10'/>
-      </div>
-      <div className={`${layout.sectionImg} flex-col`}>
-        {features.map((feature, index) => (
-          <FeatureCard key={feature.id} {...feature} index={index}/>
-        ))}
-      </div>
-    </section>*/
     <div></div>
   )
 }
